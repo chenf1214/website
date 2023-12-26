88784f0c32efc83605d014cb37ea4b39a7a75505
chenfei203 <chenfei203@jd.com>
2023-12-26 21:31:28 +0800

    feat: 部署验证

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/chenfei203/Desktop/study/website/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/chenfei203/Desktop/study/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/chenfei203/Desktop/study/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/chenfei203/Desktop/study/website/src/css/custom.css'),
+];
